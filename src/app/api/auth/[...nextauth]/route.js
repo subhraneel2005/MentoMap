a539b249b0414dbc7a61019f11be465332558cc1
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-08-19 22:13:44 +0530

    credentials signin provider added

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
-import Credentials from "next-auth/providers/credentials"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 export const authOptions = {
   
@@ -13,6 +13,26 @@ export const authOptions = {
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+        name: 'Credentials',
+
+        credentials: {
+            username: {label: 'Username', type:'text',  placeholder: "jsmith"},
+            password: {label: "Password", type: "password" },
+            email: {label: "Email", type: "email", placeholder: "jsmith@gmail.com" }
+        },
+        async authorize(credentials, req) {
+
+            const user = { name:credentials.username, email:credentials.email }
+      
+            if (user) {
+              return user
+            } 
+            else {
+              return null
+            }
+          }
     })
   ],
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -18,7 +18,13 @@ function Nav() {
   return (
     <div className='w-full px-7 py-3 bg-transparent flex justify-between top-0'>
         <h2 className='text-xl font-bold'>MentoMaps</h2>
-        {user ? <img src={user?.image} alt={user?.name} className='h-10 w-10 rounded-full shadow-xl'/> : <button onClick={() => router.push('/api/auth/signin')} className='bg-gray-300 px-7 py-2 rounded-[24px] border-2 border-gray-700  text-black hover:bg-gray-400 duration-300'>Sign in</button>}
+        {user ? 
+          <div className='flex gap-2'>
+            <img src={user?.image || '/userLogo.jpg'} alt={user?.name} className='h-10 w-10 rounded-full shadow-xl'/>
+            <p className='text-gray-600 text-[15px]'>{user?.name}</p>
+          </div> 
+        : 
+          <button onClick={() => router.push('/api/auth/signin')} className='bg-gray-300 px-7 py-2 rounded-[24px] border-2 border-gray-700  text-black hover:bg-gray-400 duration-300'>Sign in</button>}
 
         {user && <button onClick={logOut} className='bg-red-300 px-7 py-2 rounded-[24px] border-2 border-red-700  text-black hover:bg-red-400 duration-300'>Sign out</button>}
     </div>
